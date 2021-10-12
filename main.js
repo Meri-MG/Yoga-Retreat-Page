33521e6a201ec7f0db3fe5064b9f4bd4620e37b5
Meri <mgogi13@freeuni.edu.ge>
2021-10-12 13:57:43 +0400

    Add object to the js file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav-links");
+const nav = document.querySelector(".mobile-links");
 const nav_link = document.querySelectorAll(".nav-link")
 
 const show_mobile = () => {
@@ -12,11 +12,74 @@ const show_mobile = () => {
 const hide_mobile = () => {
     for (let i = 0; i < nav_link.length; i += 1){
         nav_link[i].addEventListener('click', () => {
-            nav.nav.classList.remove('nav-active');
+            nav.classList.remove('nav-active');
             hamburger.classList.remove('rotate');
         });
     }
 }
+
+const speaker = document.querySelector('.section-b');
+
+let speakers = [
+    {
+        id: '1',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    },
+    {
+        id: '2',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    },
+    {
+        id: '3',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    },
+    {
+        id: '4',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    },
+    {
+        id: '5',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    },
+    {
+        id: '6',
+        name: 'Adriene Mishler',
+        occup: 'Yoga instructor',
+        description: 'American entrepreneur, yoga teacher, and actress, who co-founded the online library of Yoga materials named ”Find What Feels Good.',
+        img: 'images/adriene.jpg',
+    }
+]
+
+function speakersContainer(obj, index){
+    let container = `<div class='speakers-wrapper'>
+        <div class='img-wrapper'>
+            <img src='${obj[index].img}' alt='yoga-teacher'></img>
+        </div>
+        <div class='info-wrapper'>
+            <h2>${obj[index].name}</h2>
+            <p class="occup">${obj[index].occup}</p>
+            <hr class='obj-hr'/>
+            <p>${obj[index].description}</p>
+        </div>
+    </div>`;
+    speaker.innerHTML += container;
+}
+
 
 show_mobile();
 hide_mobile();
